e68a63f514cd0f3ee2472ebbe9792f23fba7c105
shixudongleo <shixudongleo@gmail.com>
2024-10-11 21:02:54 +0800

    feat: add swap 4G

--- setup_scripts/ubuntu_with_docker.sh
+++ setup_scripts/ubuntu_with_docker.sh
@@ -9,6 +9,16 @@ sudo apt-get install -y zsh curl git
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 
+# setup swap
+sudo swapon --show
+sudo fallocate -l 4G /swapfile
+sudo chmod 600 /swapfile
+sudo mkswap /swapfile
+sudo swapon /swapfile
+echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab
+sudo swapon --show
+
+
 
 brew install zip unzip 
 brew install docker docker-compose docker-completion
