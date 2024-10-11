644b5384fffa7598a87790e9ff83ef301aa9820c
shixudongleo <shixudongleo@gmail.com>
2024-10-11 18:50:03 +0800

    feat: add ubuntu docker shell script

---
+++ setup_scripts/ubuntu_with_docker.sh
@@ -0,0 +1,4 @@
+# basic requirements 
+sudo apt-get install build-essential procps curl file git
+# install homebrew for linux
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
